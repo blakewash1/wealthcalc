f653474ed0b403dd16fcdd9d8e8ba6ba1af412b4
Blake Washington <blakewash1@gmail.com>
2025-06-26 18:46:57 -0400

    initial project commit

---
+++ src/test/java/com/blakewashington/wealthcalc/WealthCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blakewashington.wealthcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
