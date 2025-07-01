79f31ab3a64197f4d5e056ecba0dbce87039f3a2
Blake Washington <blakewash1@gmail.com>
2025-07-01 18:14:28 -0500

    enable cross origin resource sharing for react app orgin

--- backend/src/main/java/com/blakewashington/wealthcalc/config/SecurityConfig.java
+++ backend/src/main/java/com/blakewashington/wealthcalc/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +20,21 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(authorizeConfig -> {
                         authorizeConfig.anyRequest().permitAll();
-                }).csrf(csrf -> csrf.disable()) // disable CSRF for now
+                }).cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
+    }
+
+    // enable CORS for front-end origin
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
