48dda63efa0ec7c09dcdee9d5c7787ebaf560879
Blake Washington <blakewash1@gmail.com>
2025-07-03 00:42:57 -0500

    frontend directory and docker-compose.yml from prev commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import PlanForm from './components/PlanForm';
+
+function App() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>WealthCalc</h1>
+            <PlanForm />
+        </div>
+    );
+}
+
+export default App;
